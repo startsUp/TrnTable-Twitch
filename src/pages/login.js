72894086b188471cd14b01090b040b84bfbbca33
shardool <shardool_patel@hotmail.com>
2020-01-24 19:08:46 -0500

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,65 @@
-import React, { Component } from 'react'
+import React, { useState, Component } from 'react'
 import '../App.css'
 import AppLogo from '../components/logo'
 import ConfirmActionPopup from '../components/confirmPopup'
 import {ReactComponent as InfoLogo} from '../res/images/round-info.svg'
+
+export default function Login(props){
+    const handleLogin = () => {
+        popupCenter('https://jukebox-2952e.firebaseapp.com/login', 'Spotify Auth', 350, 550);
+    }
+
+    const popupCenter = (url, title, w, h) => {
+        // Fixes dual-screen position                         Most browsers      Firefox
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+    
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : window.screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : window.screen.height;
+    
+        var systemZoom = width / window.screen.availWidth;
+        var left = (width - w) / 2 / systemZoom + dualScreenLeft
+        var top = (height - h) / 2 / systemZoom + dualScreenTop
+        var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+        
+        // Puts focus on the newWindow
+      //  if (window.focus) newWindow.focus();
+      window.addEventListener('message', event => {
+        // IMPORTANT: check the origin of the data! 
+        if (event.origin.startsWith('https://trntable.live')) { 
+            // The data was sent from your site.
+            // Data sent with postMessage is stored in event.data:
+            newWindow.close()
+            this.props.callback(event.data)
+        } else {
+            // The data was NOT sent from your site! 
+            // Be careful! Do not use it. This else branch is
+            // here just for clarity, you usually shouldn't need it.
+            return; 
+        } 
+    	}); 
+    }
+
+    return (
+			<div className='login-page-container'>
+							{popup.show && <ConfirmActionPopup popupInfo={popup}/>}
+							<InfoLogo className='dash-logo' id='info-icon' onClick={handleClick}/> 
+				<div className='login-page-logo'>
+					<AppLogo styleName='app-logo' animate={true}/>
+					<div className='login-page-title'>TrnTable</div>
+				</div>
+				<div>
+					<div id='spotify-login-tip'>Let's Get Started</div>
+					<a className='button-container' onClick={handleLogin}>
+						<span id='spotify-button'>
+							Login with spotify
+						</span>
+					</a>
+				</div>
+			</div> 
+    )
+}
+
 class Login extends Component {
     constructor(props){
         super(props)
