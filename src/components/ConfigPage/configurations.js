d4327dc2a13e7f6593d3d012d4d413a2bb9da870
shardool <shardool_patel@hotmail.com>
2020-01-27 00:58:13 -0500

    add settings page

--- src/components/ConfigPage/configurations.js
+++ src/components/ConfigPage/configurations.js
@@ -2,9 +2,16 @@ import Setting from './model/Setting'
 import SettingType from './model/SettingType'
 
 export const DefaultBroadcasterSettings = [
-    new Setting('Delete Playlist After Stream end', 'If set to True, TrnTable will create a new playlist everytime you go live instead of using the last active playlist',
-                SettingType.BOOLEAN, false),
-    new Setting('Maximum Number of Songs viewer can Request', 'The maximum number of songs a viewer can request',
-                SettingType.NUMBER, 100),
-    new Setting('')
+    new Setting('Delete Playlist After Stream ends', 
+                'If set to True, TrnTable will create a new playlist everytime you go live instead of using the last active playlist.',
+                SettingType.BOOLEAN, 
+                false),
+    new Setting('Maximum Number of Songs viewer can Request', 
+                'The maximum number of songs a viewer can request. This can be reset during a stream.',
+                SettingType.NUMBER,                     
+                100),
+    new Setting('Voting Enabled', 
+                'Allow viewers to like or dislike the song currently playing.',
+                SettingType.BOOLEAN,
+                true),
 ]
