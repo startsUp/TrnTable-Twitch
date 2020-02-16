d9115f013165cadf1fb574bee3751a531199a6f4
shardool <shardool_patel@hotmail.com>
2020-02-15 20:35:27 -0500

    fix testing

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,16 @@ module.exports = {
         targets:{
           node:"current"
         }
-      }]
-    ]
+      }],
+      '@babel/preset-typescript',
+      '@babel/preset-react'
+    ],
+    env: {
+        test: {
+          plugins: [
+            "@babel/plugin-transform-modules-commonjs",
+            "@babel/plugin-proposal-class-properties"
+          ]
+        }
+      } 
   }
