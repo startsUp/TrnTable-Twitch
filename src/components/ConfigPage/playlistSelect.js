466c3c653e3278b6f6561e2beb6b295f3498561d
shardool <shardool_patel@hotmail.com>
2020-03-08 16:21:39 -0400

    add generic call with retry handler for spotify

--- src/components/ConfigPage/playlistSelect.js
+++ src/components/ConfigPage/playlistSelect.js
@@ -2,57 +2,60 @@ import React, { useState, useEffect } from "react"
 import { Typography, Box, List, Button, Checkbox, FormControlLabel, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
 import { SettingComponent } from './model/Setting'
 import { useSpotify } from "../../util/Spotify/spotify-context"
-import SpotifyWebApi from 'spotify-web-api-js'
 import { useAuth } from "../../auth/auth-context"
 
 const PLAYLIST_OPTION = { CREATE: true, EXISTING: false}
-const spotify  = new SpotifyWebApi();
-const LIMIT = 50
+// const spotify  = new SpotifyWebApi();
+const PLAYLIST_LIMIT = 50
 
 export const PlaylistSelect = (props) => {
-    const { classes } = props
+	const { classes } = props
 	const [option, setOption] = useState(PLAYLIST_OPTION.CREATE)
-    const auth = useAuth()
-    const [token, refreshToken] = useSpotify()
-    const [playlists, setPlaylists] = useState([])
-    const [selected, setSelected] = useState(0)
+	const auth = useAuth()
+	const [token, spotify, makeCall] = useSpotify()
+	const [playlists, setPlaylists] = useState([])
+	const [selected, setSelected] = useState(0)
 
-    const updateToken = token => spotify.setAccessToken(token)
+	const updateToken = token => spotify.setAccessToken(token)
 
-	const fetchPlaylists = (limit, offset, retry, callback=null) => {
-		spotify.getUserPlaylists({limit, offset})
-		.then(data => {
-            var playlists = data.items ? data.items : []
-            if (callback) callback(playlists)
-		}, 
-		err => {
-			if (err.status === 401 && retry < 3){
-				refreshToken().then(token => {
-                    updateToken(token)
-					fetchPlaylists(limit, offset, retry+1, callback)
-				})
-			}
-		}) 
-    }
-    
+	const fetchPlaylists = (limit, offset, callback) => {
+		makeCall(spotify.getUserPlaylists, [{limit, offset}], 
+			(data) =>{
+				var playlists = data.items ? data.items : []
+				callback(playlists)
+			},
+			err => {
+				console.log(err)
+			})
+	}           
+	
+  
 	useEffect(() =>{
-        spotify.setAccessToken(token)
-        fetchPlaylists(50, offset, 1, data => {
-            var playlists = []  
-            if (props.userSettings) { // settings were saved before                
-                setSelected(data.findIndex(p => (p.id === props.userSettings.playlistId)) + 1)
-                playlists = data.filter(p => p.id !== props.userSettings.extensionPlaylistId)
-            }
-            else{
-                playlists = data
-            }
-            setPlaylists(playlists)
-        }) 
-    }, [])
+		// spotify.setAccessToken(token)
+		fetchPlaylists(PLAYLIST_LIMIT, 0, data => {
+				var playlists = []  
+				if (props.configSet) { // settings were saved before                
+						setSelected(data.findIndex(p => (p.id === props.userSettings.playlistId)))
+						playlists = data.filter(p => p.id !== props.userSettings.extensionPlaylistId)
+				}
+				else{
+						playlists = data
+				}
+				setPlaylists(playlists)
+		}) 
+	}, [])
     
 	const handleOptionChange = () => {
-        setOption(!option)
-    }
+			props.userSettings.playlistId = option === PLAYLIST_OPTION.CREATE ? playlists[selected].id : null
+			setOption(!option)
+			console.log(props.userSettings)
+	}
+    
+	const updateSelected = (e) => {
+			setSelected(e.target.value)
+			props.userSettings.playlistId = playlists[e.target.value].id // update user settings 
+			console.log(props.userSettings)
+	}
 
 	return(
 		<List>
@@ -104,11 +107,11 @@ export const PlaylistSelect = (props) => {
                 labelId="demo-simple-select-filled-label"
                 id="demo-simple-select-filled"
                 value={selected}
-                onChange={e => setSelected(e.target.value)}
+                onChange={updateSelected}
                 >
                { 
                     playlists.map((item, index) => 
-                        <MenuItem key={item.id} value={index+1}>
+                        <MenuItem key={item.id} value={index}>
                             {item.name}
                         </MenuItem>
                     )
--- src/util/Spotify/spotify-context.js
+++ src/util/Spotify/spotify-context.js
@@ -19,25 +19,52 @@ function SpotifyProvider(props) {
   const [token, setToken] = useState(null)
   const [fetch, fetchDone] = useState(false)
   
+  const updateToken = (token) => {
+    api.setAccessToken(token)
+    setToken(token)
+  }
+
   const refreshSpotifyToken = async (id=auth.data.channelId) => {
     // fetch broadcaster data to make sure they are registered
     const token = await auth.makeAuthorizedCall(`${API_URL}/broadcaster/${id}`).then(res=>res.json())  
-    console.log('token ->', token.access_token)
+    console.log('token ->', token)
     const spotifyToken = token.access_token || null
-    setToken(spotifyToken)
+    updateToken(spotifyToken)
     fetchDone(true)
     return spotifyToken
+  }
+
+  /**
+   * 
+   */
+  const makeCall = async (f, args, onSuccess, onError) =>{
+		f(...args)
+			.then(
+				data => {
+					onSuccess(data)
+				},
+				err => {
+					if(err.status === 401){
+						refreshSpotifyToken()
+							.then(() => makeCall(f, args, callback, err))
+					}
+					else{
+						onError(err)
+					}
+				}
+			)
   }
 
   useEffect(()=>{
     if (auth.data.channelId){
       refreshSpotifyToken(auth.data.channelId)
+        .then()
     }
   }, [auth.data.channelId])
 
   if (fetch){    
     return (
-      <SpotifyContext.Provider value={[token, refreshSpotifyToken]} {...props}/>
+      <SpotifyContext.Provider value={[token, api, makeCall]} {...props}/>
     )
   }
   else {
