08fa2a8a26da49e1a33f70ebaa148f21d9540019
shardool <shardool_patel@hotmail.com>
2020-03-18 20:20:35 -0400

    now playing - add refresh button

--- src/components/NowPlaying/components/spotifyNowPlaying.js
+++ src/components/NowPlaying/components/spotifyNowPlaying.js
@@ -5,6 +5,7 @@ import { Avatar } from '@material-ui/core';
 import { getThemeProps } from '@material-ui/styles';
 import ThumbDownRoundedIcon from '@material-ui/icons/ThumbDownRounded';
 import ThumbUpRoundedIcon from '@material-ui/icons/ThumbUpRounded';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,7 +15,10 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(3),
     gridGap: theme.spacing(2),
     maxHeight: '500px'
-  },
+	},
+	icon: {
+		cursor: 'pointer'
+	},
   albumImage: {
     width: theme.spacing(10),
     height: theme.spacing(10),
@@ -33,7 +37,8 @@ const useStyles = makeStyles(theme => ({
   },
   vote: {
     fill: 'none',
-    stroke: theme.palette.primary.main
+		stroke: theme.palette.primary.main,
+		cursor: 'pointer'
   },
   selectedVote: {
     fill: theme.palette.primary.main
@@ -43,10 +48,45 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const Votes = props => {
+	const {classes, vote, handleVote} = props
+	return(
+		<Box className={classes.voting}>
+			<div className={classes.dislike}>
+				<ThumbDownRoundedIcon className={vote === Vote.DISLIKE ? classes.selectedVote : classes.vote} onClick={() => handleVote(Vote.DISLIKE)}/>
+				{/* <Typography className={classes.voteCount} variant="body2" color={vote === Vote.DISLIKE ? 'primary' : 'secondary'}>3.3k</Typography> */}
+			</div>
+			<div className={classes.like}>
+				<ThumbUpRoundedIcon className={vote === Vote.LIKE ? classes.selectedVote : classes.vote} onClick={() => handleVote(Vote.LIKE)}/>
+				{/* <Typography className={classes.voteCount} variant="body2" color={vote === Vote.LIKE ? 'primary' : 'secondary'}>3.3k</Typography> */}
+			</div>
+		</Box>
+	)
+	
+}
+const NowPlaying = props => {
+	const {classes, track} = props
+	return(
+		<div style={{display: 'contents', textAlign: 'center'}}>
+			<Avatar alt={track.album.name}
+				variant="rounded" src={track.album.images[0].url} 
+				className={classes.albumImage}
+			/>
+			<div>
+				<Typography variant="h6" color='textPrimary' align='center'>{track.name}</Typography>
+				<Typography variant="body2" color='textSecondary' align='center'>{track.artists.map(artist => artist.name).join(", ")}</Typography>
+			</div>
+		</div>
+	)
+}
+const Refresh = props => (
+	<RefreshIcon className={props.iconStyle} color='primary' onClick={props.onClick}/>
+)
 const Vote = { NONE: 1, LIKE: 2, DISLIKE: 3}
 export default function SpotifyNowPlaying(props) {
     const classes = useStyles();
     const [vote, setVote] = React.useState(Vote.NONE);
+    
     const track = {
       artists: [{
         external_urls: {spotify: "https://open.spotify.com/artist/7gP3bB2nilZXLfPHJhMdvc"},
@@ -73,26 +113,13 @@ export default function SpotifyNowPlaying(props) {
     }
 
     return(
-        <div className={classes.root}>
-          <Avatar alt={track.album.name}
-            variant="rounded" src={track.album.images[0].url} 
-            className={classes.albumImage}
-          />
-          <div>
-            <Typography variant="h6" color='textPrimary' align='center'>{track.name}</Typography>
-            <Typography variant="body2" color='textSecondary' align='center'>{track.artists.map(artist => artist.name).join(", ")}</Typography>
-          </div>
-          <Box className={classes.voting}>
-            <div className={classes.dislike}>
-              <ThumbDownRoundedIcon className={vote === Vote.DISLIKE ? classes.selectedVote : classes.vote} onClick={() => handleVote(Vote.DISLIKE)}/>
-              {/* <Typography className={classes.voteCount} variant="body2" color={vote === Vote.DISLIKE ? 'primary' : 'secondary'}>3.3k</Typography> */}
-            </div>
-            <div className={classes.like}>
-              <ThumbUpRoundedIcon className={vote === Vote.LIKE ? classes.selectedVote : classes.vote} onClick={() => handleVote(Vote.LIKE)}/>
-              {/* <Typography className={classes.voteCount} variant="body2" color={vote === Vote.LIKE ? 'primary' : 'secondary'}>3.3k</Typography> */}
-            </div>
-          </Box>
-        </div>
+			<div className={classes.root}>
+				<Refresh iconStyle={classes.icon} onClick={() => console.log('reresh')}/>
+				<NowPlaying classes={classes} track={track}/>
+				<Votes classes={classes} vote={vote} handleVote={handleVote}/>
+			</div>
     )
     
 }
+
+
