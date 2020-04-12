80bbc33af1d7932e8efd42f724bc25f50b083cd9
shardool <shardool_patel@hotmail.com>
2020-04-12 10:55:56 -0400

    Make video component same as panel

--- src/VideoComponent.js
+++ src/VideoComponent.js
@@ -3,31 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ViewerDashboard from './ViewerDashboard';
 import registerServiceWorker from './registerServiceWorker';
+import { TrnTableTheme } from './global-theme'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
+import { GraphQLAuth } from './auth/GraphQLAuth';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { AuthProvider } from './auth/auth-context';
+import { ViewType } from './util/Twitch/ViewType';
 
-
-const theme = createMuiTheme({
-    
-     typography: {
-        fontFamily: [
-            'sofia_proregular',
-            
-        ].join(','),
-        h1: {
-            fontFamily: 'sofia_problack'
-        },
-        h5: {
-            fontFamily: 'sofia_problack'
-        },
-        body2: {
-            fontFamily: 'sofia_prolight',
-        }
-   },
- });
 
 ReactDOM.render(
-<MuiThemeProvider theme={ theme }>
-    <ViewerDashboard  />
+<MuiThemeProvider theme={ TrnTableTheme }>
+  <AuthProvider viewType={ViewType.PANEL}>
+    {/* <ViewerDashboard /> LET AUTH DECIDE WHAT TO RENDER */}
+  </AuthProvider>	
 </MuiThemeProvider>
 , document.getElementById('root'));
 // registerServiceWorker();
