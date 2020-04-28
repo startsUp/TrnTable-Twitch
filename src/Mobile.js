786c161ea0b3b8414e1f891cbc0a9581e3b712b0
shardool <shardool_patel@hotmail.com>
2020-04-28 15:54:04 -0400

    make mobile active

--- src/Mobile.js
+++ src/Mobile.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ViewerDashboard from './ViewerDashboard';
 import registerServiceWorker from './registerServiceWorker';
+import { TrnTableTheme } from './global-theme'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
+import { GraphQLAuth } from './auth/GraphQLAuth';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { AuthProvider } from './auth/auth-context';
+import { ViewType } from './util/Twitch/ViewType';
 
- 
 
-ReactDOM.render(<ViewerDashboard />, document.getElementById('root'));
+ReactDOM.render(
+<MuiThemeProvider theme={ TrnTableTheme }>
+  <AuthProvider viewType={ViewType.PANEL}>
+    {/* <ViewerDashboard /> LET AUTH DECIDE WHAT TO RENDER */}
+  </AuthProvider>	
+</MuiThemeProvider>
+, document.getElementById('root'));
 // registerServiceWorker();
